f8ed2e0c3ee09e3e1ab7d11d3a616fd5bfc7dfac
virgantara <vinux.edu@gmail.com>
2024-12-26 08:05:30 +0700

    + sinta score 3yr dosen

--- restart_sinta.sh
+++ restart_sinta.sh
@@ -1,4 +1,5 @@
 docker container stop sinta-api
+docker container rm -f sinta-api
 docker image rm -f sinta-gs-api-img
 docker build -t sinta-gs-api-img .
 docker run -d --name sinta-api -p 5000:7000 sinta-gs-api-img
